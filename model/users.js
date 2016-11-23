e9d7477f099c5bfe2c492803b9a7975dc870b274
MegrezZhu <mystery490815101@gmail.com>
2016-11-23 20:57:31 +0800

    implementations.

--- model/users.js
+++ model/users.js
@@ -11,20 +11,21 @@ class User {
     }
 }
 
-const host = 'www.megres-says-hi.cn';
+const host = 'www.megrez-says-hi.cn';
 const port = 27017;
-const dbName = 'users';
-
-let donePromise;
+const dbName = 'signin';
+const username = 'root';
+const password = 'toor';
 
 const userList = new class {
     connect() {
         let that = this;
         return new Promise(function (resolve, reject) {
             client
-                .connect(`mongodb://${host}:${port}/${dbName}`)
+                .connect(`mongodb://${username}:${password}@${host}:${port}/${dbName}?authSource=admin`)
                 .then(function (db) {
                     that.db = db;
+                    that.users = db.collection('users');
                     resolve(db);
                 })
                 .catch(reject);
@@ -35,16 +36,35 @@ const userList = new class {
         let that = this;
         return new Promise(function (resolve, reject) {
             let user = new User(name, password, id, phone, mail);
+            that.users
+                .updateOne(user, user, {upsert: true})
+                .then(function (result) {
+                    resolve(result.result.n === 1);
+                })
+                .catch(reject);
         });
     }
 
-    checkUserExist(name) {
+    checkExist(param) {
+        let that = this;
+        return new Promise(function (resolve, reject) {
+            that.users
+                .find(param)
+                .count()
+                .then(count => resolve(count > 0))
+                .catch(reject);
+        });
+    }
 
+    getUser(param) {
+        let that = this;
+        return new Promise(function (resolve, reject) {
+            that.users
+                .findOne(param)
+                .then(resolve)
+                .catch(resolve);
+        });
     }
 };
 
-module.exports = {
-    User,
-    userList,
-    donePromise
-};
+module.exports = userList;
